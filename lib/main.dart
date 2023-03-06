45567639595b4f78bd8e18fbe3cec5bca87741a3
yudai <yudai0627h@gmail.com>
2023-03-06 23:12:40 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'controller.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Controller c = Get.put(Controller());
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Obx(() => Text("Clicks: ${c.count}")),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Go to Other"),
+          onPressed: () => Get.to(Other()),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            c.increment();
+          },
+          child: const Icon(Icons.add)),
+    );
+  }
+}
+
+class Other extends StatelessWidget {
+  Other({Key? key}) : super(key: key);
+  final Controller c = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("${c.count}"),
+      ),
+    );
+  }
+}
